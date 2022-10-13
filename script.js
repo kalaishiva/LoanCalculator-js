1113a834a2b21f3471493ecab4b5fca29930abd1
designhappily <65686852+shivakalai@users.noreply.github.com>
2022-10-13 12:41:33 +0200

    Loan calculator app javascript started

---
+++ script.js
@@ -0,0 +1,15 @@
+let monthlyEmi = document.querySelector(".montlyEmi");
+// console.log(monthlyEmi);
+let principalAmount = document.querySelector(".principalAmount");
+let interestAmount = document.querySelector(".interestAmount");
+let totalAmount = document.querySelector(".totalAmount");
+//console.log(principalAmount);
+
+let loanAmount = document.querySelector(".loanAmount");
+let loanTenure = document.querySelector(".loanTenure");
+let rateOfInterest = document.querySelector(".rateOfInterest");
+//console.log(loanTenure);
+let emiCalcButton = document.querySelector(".emiCalcBtn");
+
+loanAmount = loanAmount.innerHTML;
+console.log(loanAmount);
